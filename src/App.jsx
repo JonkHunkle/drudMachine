677af550745a79f5450876a9315a7f57ae04b5c6
dczer <czerwinskidavid1@gmail.com>
2023-11-05 22:45:27 -0800

    sick fix

--- src/App.jsx
+++ src/App.jsx
@@ -11,51 +11,27 @@ function App() {
   const [soundBank, setSoundBank] = useState([])
   const soundBankRef = useRef([])
 
-
-
-
-
   const togglePlay = async () => {
-    setPlaying(!playing);
-  
     if (!playing) {
+      Tone.loaded()
       loop.start();
     } else {
       loop.stop();
       counterRef.current = 1;
-      const container = document.getElementById('box');
-
-      const allPads = Array.from(container.getElementsByClassName('drum-pad'));
-      //could split into on/off beat
-      const soundBankCopy = [...soundBankRef.current];
-      allPads.forEach(pad=>{
-        const beatInSequence = pad.getAttribute('name');
-        const trackId = pad.getAttribute("data-sequence");
-        let foundTrack = soundBankCopy.find(track => trackId === track.id)
-        soundBankCopy.forEach((track, i)=>{
-          if(trackId===track.id && track.sequence[Number(beatInSequence)-1]) pad.style.backgroundColor = 'aquamarine'
-          else if(!foundTrack.sequence[Number(beatInSequence)-1]) pad.style.backgroundColor = 'white'
-        })
-       
-      })
-
+      resetPadColors()
     }
+    setPlaying(!playing);
   };
-
-
-
 
   const renderPads = (track) => {
     const drumPads = [];
     let color
-    //replace limit with variable to track number of beats (subdivisions)
     for (let i = 1; i <= 8; i++) {
       color = track.sequence[i-1]? 'aquamarine':null
         drumPads.push(<DrumPad  track={track} key={track.id+'-'+i} beat={i} updateTrackSequence={updateTrackSequence} color={color}/>);
     }
     return drumPads;
 }
-
 
 const updateTrackSequence = (e) => {
   const trackId = e.target.getAttribute("data-sequence");
@@ -74,7 +50,7 @@ const updateTrackSequence = (e) => {
 
 const addNewSequence= ()=>{
   let soundBankCopy = [...soundBank]
-  let trackVolume = new Tone.Gain(2).toDestination()
+  let trackVolume = new Tone.Gain(2).connect(masterGain)
   let newInstrument = new Tone.Synth().connect(trackVolume)
   let track ={
     sequence: new Array(8).fill(false),
@@ -82,55 +58,61 @@ const addNewSequence= ()=>{
     volume: trackVolume,
     id: uuidv4()
   }
-        soundBankCopy.push(track)
-        soundBankRef.current=soundBankCopy
-        setSoundBank(soundBankCopy)
+  soundBankCopy.push(track)
+  soundBankRef.current=soundBankCopy
+  setSoundBank(soundBankCopy)
 }
 
+const resetPadColors= ()=>{
+      const allPads = Array.from(document.getElementsByClassName('drum-pad'));
+      const soundBankCopy = [...soundBankRef.current];
+      allPads.forEach(pad=>{
+        const beatInSequence = pad.getAttribute('name');
+        const trackId = pad.getAttribute("data-sequence");
+        let foundTrack = soundBankCopy.find(track => trackId === track.id)
+        soundBankCopy.forEach((track, i)=>{
+          if(trackId===track.id && track.sequence[Number(beatInSequence)-1]) pad.style.backgroundColor = 'aquamarine'
+          else if(!foundTrack.sequence[Number(beatInSequence)-1]) pad.style.backgroundColor = 'white'
+        })
+      })
+}
 
-
-const play = useCallback((time) => {
-  const container = document.getElementById('box');
-
-  const allPads = Array.from(container.getElementsByClassName('drum-pad'));
-  //could split into on/off beat
+const play = useCallback( (time) => {
+  const allPads = Array.from(document.getElementsByClassName('drum-pad'));
   const soundBankCopy = [...soundBankRef.current];
+  
+  for (let track of soundBankCopy){
+    if(track.sequence[counterRef.current-1])
+    track.track.triggerAttackRelease('C2', '8n', time);
+  }
 
-
-  allPads.forEach(pad=>{
-    const beatInSequence = pad.getAttribute('name');
+  for (let pad of allPads){
+    const beatInSequence = Number(pad.getAttribute('name'));
     const trackId = pad.getAttribute("data-sequence");
-    let foundTrack = soundBankCopy.find(track => trackId === track.id)
-    soundBankCopy.forEach((track, i)=>{
-      if(trackId===track.id && track.sequence[Number(beatInSequence)-1]) pad.style.backgroundColor = 'aquamarine'
-      else if(!foundTrack.sequence[Number(beatInSequence)-1]) pad.style.backgroundColor = 'white'
-    })
-    if(counterRef.current===Number(beatInSequence)) {
+    let foundTrack = soundBankCopy.find(track =>track.id ===trackId)
+    if (Number(pad.getAttribute('name'))=== counterRef.current){
       pad.style.backgroundColor = 'red';
-      if(foundTrack.sequence[beatInSequence]){
-        foundTrack.track.triggerAttackRelease('C2', '8n', time);
-      }
     }
-  })
-
+    pad.style.backgroundColor = beatInSequence === counterRef.current ? 'red' : (foundTrack.sequence[beatInSequence-1]?'aquamarine':'white');
+  }
 }, []);
 
-  const playSong = useCallback((time) => {
+  const playSong = useCallback( (time) => {
     play(time);
-    counterRef.current = (counterRef.current+1) % 9;
-  },[play]);
+    counterRef.current = (counterRef.current % 8) + 1; // Update the current beat
+},[play]);
 
 
   useEffect(()=>{
-
-  if (!loop) {
-    setLoop(new Tone.Loop(playSong, "8n"));
-    Tone.loaded();
-    Tone.start();
-    Tone.Transport.timeSignature = 4 / 4;
-    Tone.Transport.start();
-  }if(!masterGain){
-    let initMaster = new Tone.Gain(0).toDestination()
+    if (!loop) {
+      Tone.start();
+      Tone.loaded();
+      setLoop(new Tone.Loop(playSong, "8n")); // update to handle dynamic subdivisions
+      Tone.Transport.timeSignature = 4 / 4;
+      Tone.Transport.start();
+  }
+  if(!masterGain){
+    let initMaster = new Tone.Gain(2).toDestination()
     initMaster.id = uuidv4()
     setMasterGain(initMaster)
     }
@@ -138,13 +120,15 @@ const play = useCallback((time) => {
     loop?.dispose();
     setPlaying(false);
     counterRef.current = 1;
+    resetPadColors()
+
   };
-},[loop, playSong, masterGain, soundBank])
+},[loop, playSong, masterGain])
 
 
   return (
     <div className='App'>
-      <button onClick={addNewSequence}>Add Track</button>
+      <button style={{fontSize:'1rem', height:'min-content'}} onClick={addNewSequence}>Add Track</button>
       {!playing?(<svg onClick={togglePlay} width="67" height="76" viewBox="0 0 67 76" fill="none" xmlns="http://www.w3.org/2000/svg">
 <path d="M63 31.0718C68.3333 34.151 68.3333 41.849 63 44.9282L12 74.3731C6.66667 77.4523 0 73.6033 0 67.4449L0 8.55514C0 2.39674 6.66667 -1.45227 12 1.62693L63 31.0718Z" fill="black"/>
 </svg>
